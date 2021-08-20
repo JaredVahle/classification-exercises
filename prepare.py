7a6bb070ff0bb9d0333e4d20b2ed6e86c0f3c153
JaredVahle <jaredjohnvahle@gmail.com>
2021-08-20 15:52:23 -0500

    adding my random forest exercises

--- prepare.py
+++ prepare.py
@@ -21,6 +21,7 @@ def clean_data(df):
     df.drop_duplicates(inplace=True)
     df.drop(columns=['deck', 'embarked', 'class'], inplace=True)
     df.embark_town.fillna(value='Southampton', inplace=True)
+    df.age.fillna(value = df.age.median(),inplace = True)
     dummy_df = pd.get_dummies(df[['sex', 'embark_town']], drop_first=True)
     return pd.concat([df, dummy_df], axis=1)
 
